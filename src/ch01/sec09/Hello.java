434fb9cf5b8570a2f789a28516aa1b9808652df1
ジェヒョン <h94051987@gmail.com>
2023-11-18 11:02:34 +0900

    231118 part.1

---
+++ src/ch01/sec09/Hello.java
@@ -0,0 +1,19 @@
+package ch01.sec09;
+/** 
+ * @author jeHyun 
+ */
+/*
+ 장제목: 1장 자바 시작하기
+ 작성일: 2023.11.18
+ */
+// 클래스 선언
+public class Hello {
+	// main() 메소드 블록, 실행 진입점.
+	public static void main(String[] args) {
+		
+		// 콘솔뷰에 출력하는 코드
+		System.out.println("Hello, Java");
+		
+	}
+
+}
