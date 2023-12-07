dabccd83c2f54753cb02f23d1e552cd56b1de069
ジェヒョン <h94051987@gmail.com>
2023-12-07 22:48:51 +0900

    231207

--- src/ch05/sec10/AdvancedForExample.java
+++ src/ch05/sec10/AdvancedForExample.java
@@ -8,10 +8,12 @@ public class AdvancedForExample {
 		
 		// 배열 항목 전체 합 구하기
 		int sum = 0;
-		for (int score : scores) { // 5개의 항목이 한 번씩 score변수에 저장되고 sum에 누적됨. (반복횟수 5)
+		for (int score : scores) { 
+			// 5개의 항목이 한 번씩 score변수에 저장되고 sum에 누적됨. (반복횟수 5)
 			sum = sum + score;
 		}
 		System.out.println("점수 총합 = " + sum);
+		
 		// 배열 항목 전체 평균 구하기
 		double avg = (double) sum / scores.length;
 		System.out.println("점수 평균 = " + avg);
---
+++ src/ch05/sec12/Week.java
@@ -0,0 +1,14 @@
+package ch05.sec12;
+
+// 열거 타입 이름 Week
+public enum Week {
+	// 열거 상수 목록(한정된 값 목록)
+	// 관례적으로 모두 대문자로 작성, 여러 단어로 구성되면 언더바로 연결하면 된다.
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
